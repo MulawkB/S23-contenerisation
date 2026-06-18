72d1e85ccdbd60994d25f0f9d2fde11adf0ec7a1
Brian Mulawka <mulawkabrian@gmail.com>
2026-06-18 09:57:39 +0200

    fix 2

--- Payment/payment-service/src/datasources/paymentdb.datasource.ts
+++ Payment/payment-service/src/datasources/paymentdb.datasource.ts
@@ -2,18 +2,21 @@ import {inject, lifeCycleObserver, LifeCycleObserver} from '@loopback/core';
 import {juggler} from '@loopback/repository';
 
 const config = {
-  name: 'db',
+  name: 'paymentdb',
   connector: 'mongodb',
   url: `mongodb://${process.env.MONGO_HOST}:27017/paymentdb`,
 };
 
 @lifeCycleObserver('datasource')
-export class DbDataSource extends juggler.DataSource implements LifeCycleObserver {
+export class PaymentdbDataSource
+  extends juggler.DataSource
+  implements LifeCycleObserver
+{
   static dataSourceName = 'paymentdb';
   static readonly defaultConfig = config;
 
   constructor(
-    @inject('datasources.config.db', {optional: true})
+    @inject('datasources.config.paymentdb', {optional: true})
     dsConfig: object = config,
   ) {
     super(dsConfig);
